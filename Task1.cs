2af1ae75ef995fcc9acaa6473f32009494002377
Napoleonsha <amnoli3847@gmail.com>
2025-12-20 14:39:24 +0200

    First tasks

---
+++ Task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lessons
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            float firstNumber, secondNumber, result;
+
+            Console.WriteLine("Введите первое число");
+            bool firstNumValid = float.TryParse(Console.ReadLine(), out firstNumber);
+
+            Console.WriteLine("Введите второе число");
+            bool secondNumValid = float.TryParse(Console.ReadLine(), out secondNumber b);
+
+            if (firstNumValid && secondNumValid) {
+                result = (firstNumber + secondNumber) / 2;
+                Console.WriteLine("Среднее арифметическое ваших чисел: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Введена неправильная информация");
+            }
+        }
+    }
+}
