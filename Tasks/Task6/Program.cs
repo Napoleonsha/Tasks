d6cf499cc921076b96766cec76b0355eaeaec10a
Napoleonsha <amnoli3847@gmail.com>
2026-01-03 19:12:36 +0200

    Task6
    Подсчет количества четных и нечетных чисел в заданном пользователем диапазоне

---
+++ Tasks/Task6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, 
+                count = 0,
+                even = 0,
+                odd = 0;
+            Console.WriteLine("Введите диапазон: ");
+            bool resConvert = int.TryParse(Console.ReadLine(), out number);
+            if (!resConvert)
+            {
+                Console.WriteLine("Ошибка ввода данных");
+            }
+            else
+            {
+               while (count < number)
+                {
+                    count++;
+                    if (count % 2 == 0) 
+                    {
+                        even++;
+                    } 
+                    else
+                    {
+                        odd++;
+                    }
+                }
+                Console.WriteLine("В заданном диапазоне {0} четных чисел и {1} нечетных чисел", even,odd);
+            }
+        }
+    }
+}
