687f1e5ecbfd3626084436ceaeb478be0d437d02
Napoleonsha <amnoli3847@gmail.com>
2026-01-11 15:23:41 +0200

    Task7

---
+++ Tasks/Task7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int j = 0; j <= 10; j++)
+                {
+                    for(int k = 0; k <= j; k++)
+                    {
+                        Console.Write('#');
+                    }
+                    Console.WriteLine();
+                }
+
+            Console.WriteLine();
+
+            for (int j = 10; j >= 0; j--)
+            {
+                for (int k = 0; k <= j; k++)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            for (int j = 0; j <= 10; j++)
+            {
+                for (int k = 0; k <= j; k++)
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 10; k >= j; k--)
+                {
+                    Console.Write('#');
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int j = 0; j <= 10; j++)
+            {
+                for (int k = 10; k >= j; k--)
+                {
+                    Console.Write(' ');
+                }
+                for (int k = 0; k <= j; k++)
+                {
+                    Console.Write('#');
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
